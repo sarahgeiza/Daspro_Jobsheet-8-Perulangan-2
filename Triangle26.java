870eff793e71d735cbac808a16acc387510b3b5a
sarahgeiza <sarahgeizask@gmail.com>
2025-10-31 10:41:55 +0700

    Perubahan output segitiga

--- Triangle26.java
+++ Triangle26.java
@@ -4,13 +4,14 @@ public class Triangle26 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai n = ");
         int n = sc.nextInt();
-        int i = 0;
+        int i = 1;
         while (i <= n) {
-            int j = 0;
-            while (j < 1) {
+            int j = 1;
+            while (j <= i) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
     }
